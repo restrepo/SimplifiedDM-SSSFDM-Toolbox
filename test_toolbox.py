5f49d3bb34b7bd1a0295fd7b3bae17550b531e78
Diego Restrepo <rescolo@gmail.com>
2015-03-29 17:53:02 -0500

    --yes option added to butler

--- test_toolbox.py
+++ test_toolbox.py
@@ -1,10 +1,11 @@
+MODEL='radinert' #'SSDM'
 import os.path
 import commands
 def test_micromegas():
-    assert os.path.isfile('SPHENO/bin/SPhenoSSDM'), 'run ./butler SSDM first'
-    a=commands.getoutput('SPHENO/bin/SPhenoSSDM SPHENO/SSDM/LesHouches.in.SSDM')
-    assert os.path.isfile('SPheno.spc.SSDM')
-    a=commands.getoutput('micromegas/SSDM/CalcOmega SPheno.spc.SSDM')
+    assert os.path.isfile('SPHENO/bin/SPheno%s' %MODEL), 'run ./butler %s first' %MODEL
+    a=commands.getoutput('SPHENO/bin/SPheno%s SPHENO/%s/LesHouches.in.%s' %(MODEL,MODEL,MODEL))
+    assert os.path.isfile('SPheno.spc.%s' %MODEL)
+    a=commands.getoutput('micromegas/%s/CalcOmega SPheno.spc.%s' %(MODEL,MODEL))
     assert eval(a.split('\n')[-1].split('%')[0])==4, 'micrOMEGAS failed'
     
 if __name__=='__main__':
