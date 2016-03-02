6bf82c195d04d1bdba0e5fb153b29d118adf1cd6
restrepo <restrepo@udea.edu.co>
2016-03-02 18:21:59 +0000

    Fix micromegas

--- madgraph/Template/NLO/Utilities/appl_interface_dummy.f
+++ madgraph/Template/NLO/Utilities/appl_interface_dummy.f
@@ -1,1 +1,34 @@
-../SubProcesses/appl_interface_dummy.f
+      subroutine APPL_fill
+      implicit none
+      write(6,*) "You should not be here in APPL_fill dummy!"
+      stop
+      return
+      end
+c
+      subroutine APPL_fill_ref_out
+      implicit none
+      write(6,*) "You should not be here in APPL_fill dummy!"
+      stop
+      return
+      end
+c
+      subroutine APPL_fill_ref
+      implicit none
+      write(6,*) "You should not be here in APPL_fill dummy!"
+      stop
+      return
+      end
+c
+      subroutine APPL_init
+      implicit none
+      write(6,*) "You should not be here in APPL_init dummy!"
+      stop
+      return
+      end
+c
+      subroutine APPL_term
+      implicit none
+      write(6,*) "You should not be here in APPL_term dummy!"
+      stop
+      return
+      end
--- madgraph/Template/NLO/Utilities/open_output_files_dummy.f
+++ madgraph/Template/NLO/Utilities/open_output_files_dummy.f
@@ -1,1 +1,20 @@
-../SubProcesses/open_output_files_dummy.f
+      subroutine rinit(string)
+      character*(*) string
+      write (*,*) 'rinit (open_output_files_dummy.f) should'/
+     $     /' not be called'
+      stop
+      end
+
+
+      subroutine rwrit()
+      write (*,*) 'rwrit (open_output_files_dummy.f) should'/
+     $     /' not be called'
+      stop
+      end
+
+
+      subroutine rclos()
+      write (*,*) 'rclos (open_output_files_dummy.f) should'/
+     $     /' not be called'
+      stop
+      end
