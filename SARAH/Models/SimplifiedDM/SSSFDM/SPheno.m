e3c7b0395865b68232f8755541b9049860e8629f
Diego Restrepo <restrepo@udea.edu.co>
2016-10-19 23:41:21 -0500

    IR

---
+++ SARAH/Models/SimplifiedDM/SSSFDM/SPheno.m
@@ -0,0 +1,40 @@
+OnlyLowEnergySPheno = True;
+
+
+MINPAR={{1,Lambda1IN},
+        {2,LamSHIN},
+        {3,LamSIN},
+        {4,MSinput},
+	{5,MDFinput}
+        };
+
+
+ParametersToSolveTadpoles = {mu2};
+
+BoundaryLowScaleInput={
+   {v, vSM}, 
+ {Ye, YeSM},
+ {Yd, YdSM},
+ {Yu, YuSM},
+ {g1, g1SM},
+ {g2, g2SM},
+ {g3, g3SM},
+  {Lambda1,Lambda1IN},
+  {LamSH,LamSHIN},
+  {LamS,LamSIN},
+  {MS2, MSInput},
+  {MDF,MDFinput}(*,
+  {Ys,LHInput[Ys]}*)
+
+};
+
+
+
+
+ListDecayParticles = {Fu,Fe,Fd(*,Fre*),hh};
+ListDecayParticles3B = {{Fu,"Fu.f90"},{Fe,"Fe.f90"},{Fd,"Fd.f90"}};
+
+FlagLoopContributions = True;  
+
+DefaultInputValues ={Lambda1IN -> 0.28, LamSHIN -> 0.01, LamSIN -> 0, MSinput -> 200,
+		    MDFinput -> 400(*,Ysinput[a_] -> 0.1*)};
