c8ef2c64e7560a61533033fff6345906eda35ace
Diego Restrepo <diego.restrepo@gmail.com>
2016-11-27 23:41:35 -0500

    Fix MS2Input

--- SARAH/Models/SimplifiedDM/SSSFDM/SimplifiedDM-SSSFDM.m
+++ SARAH/Models/SimplifiedDM/SSSFDM/SimplifiedDM-SSSFDM.m
@@ -16,36 +16,33 @@ Model`Date = "2015-11-16";
 (*   Particle Content*)
 (*-------------------------------------------*)
 
+
 (* Global Symmetries *)
-
 Global[[1]] = {Z[2], Z2};
-
 
 (* Gauge Groups *)
 
-
-
-
-
-Gauge[[1]]={B,   U[1], hypercharge, g1,False,1};
-Gauge[[2]]={WB, SU[2], left,        g2,True,1};
-Gauge[[3]]={G,  SU[3], color,       g3,False,1};
+Gauge[[1]]={B,   U[1], hypercharge, g1, False, 1};
+Gauge[[2]]={WB, SU[2], left,        g2, True,  1};
+Gauge[[3]]={G,  SU[3], color,       g3, False, 1};
 
 
 (* Matter Fields *)
 
-FermionFields[[1]] = {q, 3, {uL, dL},     1/6, 2,  3,1};  
-FermionFields[[2]] = {l, 3, {vL, eL},    -1/2, 2,  1,1};
-FermionFields[[3]] = {d, 3, conj[dR],     1/3, 1, -3,1};
-FermionFields[[4]] = {u, 3, conj[uR],    -2/3, 1, -3,1};
-FermionFields[[5]] = {e, 3, conj[eR],       1, 1,  1,1};
-FermionFields[[6]] = {rd, 1, conj[ed],      1, 1,  1,-1};
-FermionFields[[7]] = {ru, 1, eu,           -1, 1,  1,-1};
+FermionFields[[1]] = {q, 3, {uL, dL},     1/6, 2,  3, 1};  
+FermionFields[[2]] = {l, 3, {vL, eL},    -1/2, 2,  1, 1};
+FermionFields[[3]] = {d, 3, conj[dR],     1/3, 1, -3, 1};
+FermionFields[[4]] = {u, 3, conj[uR],    -2/3, 1, -3, 1};
+FermionFields[[5]] = {e, 3, conj[eR],       1, 1,  1, 1};
 
-ScalarFields[[1]] =  {H, 1, {Hp, H0},     1/2, 2,  1,1};
+(* Extra Fermion Field *)
+FermionFields[[6]] = {rd, 1, ed,  1, 1,  1, -1};
+FermionFields[[7]] = {ru, 1, eu,  -1, 1,  1, -1};
+
+
+ScalarFields[[1]] =  {H, 1, {Hp, H0},     1/2, 2,  1, 1};
 ScalarFields[[2]] =  {S, 1, ss,     0, 1,  1, -1};
 RealScalars = {S};
-
 
 
         
@@ -76,19 +73,23 @@ DEFINITION[EWSB][GaugeSector] =
   {{VWB[1],VWB[2]},{VWp,conj[VWp]},ZW}
 };     
         
-        
-          	
+                  	
 
 (* ----- VEVs ---- *)
 
 DEFINITION[EWSB][VEVs]= 
-{    {H0, {v, 1/Sqrt[2]}, {Ah, \[ImaginaryI]/Sqrt[2]},{hh, 1/Sqrt[2]}}     };
+{
+    {H0, {v, 1/Sqrt[2]}, {Ah, \[ImaginaryI]/Sqrt[2]},{hh, 1/Sqrt[2]}}     
+};
  
 
-DEFINITION[EWSB][MatterSector]=   
-    {{{{dL}, {conj[dR]}}, {{DL,Vd}, {DR,Ud}}},
-     {{{uL}, {conj[uR]}}, {{UL,Vu}, {UR,Uu}}},
-     {{{eL}, {conj[eR]}}, {{EL,Ve}, {ER,Ue}}}};  
+(DEFINITION[EWSB][MatterSector]=   
+{
+    {{vL}, {VL, Vv}},
+    {{{dL}, {conj[dR]}}, {{DL,Vd}, {DR,Ud}}},
+    {{{uL}, {conj[uR]}}, {{UL,Vu}, {UR,Uu}}},
+    {{{eL}, {conj[eR]}}, {{EL,Ve}, {ER,Ue}}}
+};  
 
 
 (*------------------------------------------------------*)
@@ -99,8 +100,9 @@ DEFINITION[EWSB][DiracSpinors]={
  Fd ->{  DL, conj[DR]},
  Fe ->{  EL, conj[ER]},
  Fu ->{  UL, conj[UR]},
- Fre ->{  eu, ed}, 
- Fv ->{  vL, 0}};
+ Fv ->{  VL, 0}
+ Fre ->{ eu, conj[ed]}
+ };
 
 DEFINITION[EWSB][GaugeES]={
  Fd1 ->{  FdL, 0},
